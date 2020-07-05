4c4cf805d5fc22c83507b62dda834f743bb5c3dc
tomka <tomas.katz@mail.huji.ac.il>
2020-07-05 16:52:24 +0300

    16:52 5/7

---
+++ game_state.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+WIDTH = 16
+HEIGHT = 16
+START_SCORE = 0
+CITIZEN = 1
+CORONA_ILL = 2
+WALL = 3
+EMPTY_LOCATION = '_'
+
+
+class GameState(object):
+    def __init__(self):
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.score = START_SCORE
+
+    def create_board(self):
+        board = np.array([[EMPTY_LOCATION, EMPTY_LOCATION],
+                          [WALL, WALL, WALL, EMPTY_LOCATION, WALL, WALL, WALL,
+                           WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,
+                           WALL, WALL]])
+        for i in range(2):
+            for j in range(2):
+                print(board[i][j])
+
+ga = GameState()
+ga.create_board()
