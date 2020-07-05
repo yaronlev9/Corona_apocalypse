077bbdc98f7410964051cb6ac7f0314446e0adf2
tomka <tomas.katz@mail.huji.ac.il>
2020-07-05 18:08:48 +0300

    after board

--- game_state.py
+++ game_state.py
@@ -5,8 +5,21 @@ HEIGHT = 16
 START_SCORE = 0
 CITIZEN = 1
 CORONA_ILL = 2
-WALL = 3
+WALL = '-'
 EMPTY_LOCATION = '_'
+TARGET = 'W'
+
+
+def create_board():
+    board = np.array([['_______________' + '1'], ['---_' + '-' * 12],
+                      ['---_' + '-' * 12], ['---_---_____----'],
+                      ['________---_----'], ['_--_-----_-_----'],
+                      ['_-__-_____-_----'], ['--_--_____-_____'],
+                      ['___-__-------_--'], ['_----_______-_--'],
+                      ['______-_-_---_--'], ['---_-_-_-_______'],
+                      ['____-_-_-_---_--'], ['____-_-_-___-_--'],
+                      ['_----_-_-----_--'], ['W_____-_______--']])
+    return board
 
 
 class GameState(object):
@@ -14,16 +27,8 @@ class GameState(object):
         self.width = WIDTH
         self.height = HEIGHT
         self.score = START_SCORE
-
-    def create_board(self):
-        board = np.array([[EMPTY_LOCATION, EMPTY_LOCATION],
-                          [WALL, WALL, WALL, EMPTY_LOCATION, WALL, WALL, WALL,
-                           WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL,
-                           WALL, WALL]])
-        for i in range(2):
-            for j in range(2):
-                print(board[i][j])
+        self.board = create_board()
 
 
 ga = GameState()
-ga.create_board()
+create_board()
