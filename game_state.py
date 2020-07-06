e23bd8fae66e5176dcc18afb437829bd5e058b00
tomka <tomas.katz@mail.huji.ac.il>
2020-07-06 14:48:10 +0300

    after legal actions

--- game_state.py
+++ game_state.py
@@ -7,36 +7,113 @@ class Action(Enum):
     DOWN = "DOWN"
     LEFT = "LEFT"
     RIGHT = "RIGHT"
+    STOP = "STOP"
 
 
 WIDTH = 16
 HEIGHT = 16
 START_SCORE = 0
-CITIZEN = 1
-CORONA_ILL = 2
+CITIZEN = '0'
+CORONA_ILL_1 = '1'
+CORONA_ILL_2 = '2'
 WALL = '-'
 EMPTY_LOCATION = '_'
 TARGET = 'W'
 
 
 def create_board():
-    board = np.array([['_______________' + '1'], ['---_' + '-' * 12],
+    board = np.array([['_______________' + '0'], ['---_' + '-' * 12],
                       ['---_' + '-' * 12], ['---_---_____----'],
                       ['________---_----'], ['_--_-----_-_----'],
-                      ['_-__-_____-_----'], ['--_--_____-_____'],
+                      ['_-__-1____-_----'], ['--_--_____-_____'],
                       ['___-__-------_--'], ['_----_______-_--'],
                       ['______-_-_---_--'], ['---_-_-_-_______'],
-                      ['____-_-_-_---_--'], ['____-_-_-___-_--'],
+                      ['____-_-_-_---_--'], ['2___-_-_-___-_--'],
                       ['_----_-_-----_--'], ['W_____-_______--']])
     return board
 
 
 class GameState(object):
     def __init__(self):
-        self.width = WIDTH
-        self.height = HEIGHT
-        self.score = START_SCORE
-        self.board = create_board()
+        self.__width = WIDTH
+        self.__height = HEIGHT
+        self.__score = START_SCORE
+        self.__board = create_board()
+        self.__location = (0, self.__width - 1)
+        self.__corona_1_location = (6, 5)
+        self.__corona_2_location = (13, 0)
+        self.__target = (self.__height - 1, 0)
+        self.__done = False
+        self.__mask = False
+
+    def _is_right_legal_action(self, location, player):
+        if location[1] >= self.__width:
+            return False
+        if player == 0:
+            if self.__mask:
+                if self.__board[location[0]][location[1]] == '-':
+                    return False
+                return True
+            return self.__board[location[0]][location[1]] == '_' or \
+                   self.__board[location[0]][location[1]] == 'm'
+        return self.__board[location[0]][location[1]] == '_'
+
+    def _is_left_legal_action(self, location, player):
+        if location[1] < 0:
+            return False
+        if player == 0:
+            if self.__mask:
+                if self.__board[location[0]][location[1]] == '-':
+                    return False
+                return True
+            return self.__board[location[0]][location[1]] == '_' or \
+                   self.__board[location[0]][location[1]] == 'm'
+        return self.__board[location[0]][location[1]] == '_'
+
+    def _is_up_legal_action(self, location, player):
+        if location[0] < 0:
+            return False
+        if player == 0:
+            if self.__mask:
+                if self.__board[location[0]][location[1]] == '-':
+                    return False
+                return True
+            return self.__board[location[0]][location[1]] == '_' or \
+                   self.__board[location[0]][location[1]] == 'm'
+        return self.__board[location[0]][location[1]] == '_'
+
+    def _is_down_legal_action(self, location, player):
+        if location[0] >= self.__height:
+            return False
+        if player == 0:
+            if self.__mask:
+                if self.__board[location[0]][location[1]] == '-':
+                    return False
+                return True
+            return self.__board[location[0]][location[1]] == '_' or \
+                   self.__board[location[0]][location[1]] == 'm'
+        return self.__board[location[0]][location[1]] == '_'
+
+    def get_legal_actions(self, player):
+        legal_actions = []
+        if player == 0:
+            location = self.__location
+        elif player == 1:
+            location = self.__corona_1_location
+        elif player == 2:
+            location = self.__corona_2_location
+        else:
+            raise Exception("Illegal agent index.")
+        if self._is_right_legal_action((location[0], location[1] + 1), player):
+            legal_actions.append(Action.RIGHT)
+        if self._is_left_legal_action((location[0], location[1] - 1), player):
+            legal_actions.append(Action.LEFT)
+        if self._is_up_legal_action((location[0] - 1, location[1]), player):
+            legal_actions.append(Action.UP)
+        if self._is_down_legal_action((location[0] + 1, location[1]), player):
+            legal_actions.append(Action.DOWN)
+        legal_actions.append(Action.STOP)
+        return legal_actions
 
 
 ga = GameState()
